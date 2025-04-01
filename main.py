b8119226b581adb93b1601340ed4c283e65c711a
Пользователь <shaban_ak23@nuwm.edu.ua>
2025-04-01 03:36:54 +0200

    Adding new filles to the repo.

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ window.title("Simple Text Editor")
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
-text_edit = tk.Text(window)
+txt_edit = tk.Text(window)
 frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
 btn_open = tk.Button(frm_buttons, text="Open")
 btn_save = tk.Button(frm_buttons, text="Save as...")
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+
+frm_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+window.mainloop()
